f956aeea5e6ffb541ee7f2d0c2beebc3b39d9c5c
dikamuharman <dika1254@gmail.com>
2022-03-11 23:33:35 +0700

    add link social media and change image size

--- components/Section/FooterSection.tsx
+++ components/Section/FooterSection.tsx
@@ -2,16 +2,34 @@ import React from 'react'
 import Image from 'next/image'
 import Container from '../Container'
 import DscLogo from '../../assets/logo.png'
-import { GrTwitter, GrFacebook, GrInstagram } from 'react-icons/gr'
+import { GrYoutube, GrFacebook, GrInstagram } from 'react-icons/gr'
 
 const FooterSection = () => {
   return (
     <Container as="footer" className="mt-10 flex flex-col items-center py-16">
-      <Image src={DscLogo} />
+      <Image src={DscLogo} width={300} height={150} />
       <div className="mt-10 flex w-1/2 justify-between lg:w-1/6">
-        <GrTwitter color="#405274" size={20} />
-        <GrInstagram color="#405274" size={20} />
-        <GrFacebook color="#405274" size={20} />
+        <a
+          href=" https://www.youtube.com/channel/UCpSv2Al-xgyHEnyqkCUq1kg"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <GrYoutube color="#405274" size={20} />
+        </a>
+        <a
+          href="https://www.facebook.com/dscindonesia/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <GrFacebook color="#405274" size={20} />
+        </a>
+        <a
+          href="https://www.instagram.com/gdscindonesia/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <GrInstagram color="#405274" size={20} />
+        </a>
       </div>
     </Container>
   )
