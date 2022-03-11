af35883b3ee218d3695078464854de632dc580a2
dikamuharman <dika1254@gmail.com>
2022-03-11 23:33:16 +0700

    change bg when scroll

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,13 +1,30 @@
 import { Link } from 'react-scroll'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import menus from '../../data/menus'
-import Button from '../Buttons'
 import Logo from '../icons/Logo'
 
 const Navbar = () => {
+  const [changeColor, setChangeColor] = useState(false)
+  const changeColorNavbar = () => {
+    if (window.scrollY >= 80) setChangeColor(true)
+    else setChangeColor(false)
+  }
+
+  useEffect(function mount() {
+    window.addEventListener('scroll', changeColorNavbar)
+
+    return function unMount() {
+      window.removeEventListener('scroll', changeColorNavbar)
+    }
+  }, [])
+
   return (
-    <nav className="container mx-auto mt-2 flex flex-col px-5 pt-8 lg:mt-7 lg:h-auto lg:flex-row lg:justify-between  lg:px-20">
-      <a className="h-[24px] self-center" href="/">
+    <nav
+      className={` ${
+        changeColor && 'lg:bg-white lg:shadow-md '
+      } lg:px-20" container top-0 z-50 mx-auto mt-2 flex flex-col px-16 pt-8 pb-2 lg:sticky lg:mt-7 lg:h-auto lg:flex-row lg:justify-between`}
+    >
+      <a className="h-[24px] self-center lg:self-auto " href="/">
         <Logo />
       </a>
 
@@ -27,7 +44,9 @@ const Navbar = () => {
           ))}
           <li className="ml-7">
             <a
-              href="#"
+              href="https://s.id/TelegramGDSCHackfest"
+              rel="noopener noreferrer"
+              target="_blank"
               className="cursor-pointer rounded-xl border-2 border-primary px-4 py-[10px] text-primary transition-all hover:bg-primary hover:text-white hover:shadow-md lg:px-6 lg:py-[15px]"
             >
               Join Forum Discussion
