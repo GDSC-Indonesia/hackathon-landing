f78cd54e9ae1a6b8b4cd5b5c1292e48a0b091456
dikamuharman <dika1254@gmail.com>
2022-03-11 08:39:39 +0700

    add id params in container component

--- components/Container/index.tsx
+++ components/Container/index.tsx
@@ -4,14 +4,16 @@ type ContainerProps = {
   as: 'div' | 'section' | 'main' | 'footer'
   children: React.ReactNode
   className?: string
+  id?: string
 }
 
-const Container = ({ as = 'div', children, className }: ContainerProps) => {
+const Container = ({ as = 'div', children, className, id }: ContainerProps) => {
   const Component = as || 'div'
 
   return (
     <Component
       className={`container mx-auto px-5 pt-8 md:px-16 lg:px-20 ${className}`}
+      id={id}
     >
       {children}
     </Component>
