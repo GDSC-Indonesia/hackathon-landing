86b74daea977b1cd6afd7fa5260ca53469808ef4
dikamuharman <dika1254@gmail.com>
2022-03-11 08:41:26 +0700

    add id object

--- data/menus.ts
+++ data/menus.ts
@@ -1,24 +1,34 @@
 type Menu = {
   name: string
   url: string
+  to: string
 }
 
 const menus: Array<Menu> = [
   {
     name: 'About us',
     url: '/',
+    to: 'about-us',
   },
   {
     name: 'Timeline',
     url: '/',
+    to: 'timeline',
   },
   {
     name: 'Prize',
     url: '/',
+    to: 'prize',
+  },
+  {
+    name: 'Solution challenge',
+    url: '/',
+    to: 'solution',
   },
   {
     name: 'FAQ',
     url: '/',
+    to: 'faq',
   },
 ]
 
