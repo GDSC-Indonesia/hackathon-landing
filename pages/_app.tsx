ddadb5fcd123e834ffe751c1309a61aa2bc1bd3d
dikamuharman <dika1254@gmail.com>
2022-03-12 12:41:41 +0700

    move cdn font to document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>GDSC - Hackfest Indonesia</title>
-        <link
-          href="http://fonts.cdnfonts.com/css/google-sans"
-          rel="stylesheet"
-        />
-      </Head>
       <Component {...pageProps} />
     </>
   )
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,13 @@ class Document extends NextDocument<Props> {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <title>GDSC - Hackfest Indonesia</title>
+          <link
+            href="http://fonts.cdnfonts.com/css/google-sans"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
