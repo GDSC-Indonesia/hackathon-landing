1be51e568fdaab24697bc17d6b013ed9026d27fe
James Chandra <tweenlicious@gmail.com>
2022-03-19 19:00:58 +0700

    chore: change favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,7 @@ class Document extends NextDocument<Props> {
       <Html>
         <Head>
           <title>GDSC - Hackfest Indonesia</title>
+          <link rel="shortcut icon" href="https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_180,q_auto:good,w_180/v1/gcs/platform-data-dsc/contentbuilder/favicon.png" />
           <link
             href="http://fonts.cdnfonts.com/css/google-sans"
             rel="stylesheet"
